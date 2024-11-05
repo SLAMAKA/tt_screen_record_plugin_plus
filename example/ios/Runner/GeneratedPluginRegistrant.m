72b919469606bb4b8c64d8131a301179a062b355
WIN-PJ6RQGBMU5U\User <rakish.shalkar@gmail.com>
2024-11-05 14:06:25 +0500

    initial commit

---
+++ example/ios/Runner/GeneratedPluginRegistrant.m
@@ -0,0 +1,28 @@
+//
+//  Generated file. Do not edit.
+//
+
+// clang-format off
+
+#import "GeneratedPluginRegistrant.h"
+
+#if __has_include(<integration_test/IntegrationTestPlugin.h>)
+#import <integration_test/IntegrationTestPlugin.h>
+#else
+@import integration_test;
+#endif
+
+#if __has_include(<tt_screen_record_plugin/TtScreenRecordPlugin.h>)
+#import <tt_screen_record_plugin/TtScreenRecordPlugin.h>
+#else
+@import tt_screen_record_plugin;
+#endif
+
+@implementation GeneratedPluginRegistrant
+
++ (void)registerWithRegistry:(NSObject<FlutterPluginRegistry>*)registry {
+  [IntegrationTestPlugin registerWithRegistrar:[registry registrarForPlugin:@"IntegrationTestPlugin"]];
+  [TtScreenRecordPlugin registerWithRegistrar:[registry registrarForPlugin:@"TtScreenRecordPlugin"]];
+}
+
+@end
